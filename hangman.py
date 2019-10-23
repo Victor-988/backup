3ebc817c629f093f04bd44a6298bb3c4555b94b1
Victor-988 <56047125+Victor-988@users.noreply.github.com>
2019-10-23 13:25:43 -0700

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,61 @@
+
+
+
+
+if choice == myWorld:
+	print("it's a match")
+	else:
+		print("not a match")
+
+		letter = input("Type a letter")
+		if letter in myWorld:
+			print("Letter is in the word")
+			
+
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+whileTrue:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
